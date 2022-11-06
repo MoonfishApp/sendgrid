8a202b0f949f6f9f06373698073f502397a31679
Ronald Mannak <ronaldmannak@me.com>
2022-11-06 01:07:11 -0800

    Use Moonfish version of Sendgrid-Kit

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor-community/sendgrid-kit.git", from: "1.0.0"),
+        .package(url: "https://github.com/moonfishapp/sendgrid-kit.git", branch: "master"),
     ],
     targets: [
         .target(name: "SendGrid", dependencies: [
